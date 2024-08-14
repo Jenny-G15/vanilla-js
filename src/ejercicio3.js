923c9214959b118f48cb80f98be6e3ea3679edf6
Jenny-G15 <jguadamuz@fwdcostarica.com>
2024-08-14 15:34:21 -0600

    cambios

--- src/ejercicio3.js
+++ src/ejercicio3.js
@@ -1,6 +1,32 @@
 // Inserte el código aquí
+const arreglo = ["Juan", "Maria", "Jenny"]
 
+const clonarArreglo = [arreglo]
 
+clonarArreglo.push["Daniel"]
 
+console.log(
+    arreglo,
+    clonarArreglo
+);
 // NO MODIFICAR
-export { clonarArreglo, clonarObjeto, sumar, saludar };
+const persona  = {
+    nombre: 'Maria',
+    edad: 20,
+    peso: 80
+}
+
+const persona2 = JSON.parse(JSON.stringify(persona));
+
+console.log(persona2);
+
+
+function sumar(params) {
+    
+}
+
+
+ 
+
+
+
